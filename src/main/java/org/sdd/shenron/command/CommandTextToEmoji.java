3499be03471946caa3840fc0308ee3d87fe862cd
Adrien Navratil <adrien1975@live.fr>
2016-12-11 15:59:52 +0100

    TextToEmoji can now repeat the same emoji and put the unknown character as is in the final message

--- src/main/java/org/sdd/shenron/command/CommandTextToEmoji.java
+++ src/main/java/org/sdd/shenron/command/CommandTextToEmoji.java
@@ -5,7 +5,9 @@ import fr.litarvan.krobot.command.message.MessageCommandCaller;
 import fr.litarvan.krobot.motor.Conversation;
 import fr.litarvan.krobot.motor.discord.DiscordConversation;
 import fr.litarvan.krobot.util.TextEmoji;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import joptsimple.internal.Strings;
 import org.jetbrains.annotations.NotNull;
@@ -49,15 +51,50 @@ public class CommandTextToEmoji extends ShenronCommand
             return;
         }
 
-        String message = Strings.join(args, " ");
-        TextEmoji[] toEmoji = TextEmoji.Companion.toEmoji(message);
-        String result = "";
-
-        for (TextEmoji emoji : toEmoji)
+        if (CHAR_TO_EMOJI == null)
         {
-            result += emoji.getUnicode() + " ";
+            ((MessageCommandCaller) caller).getConversation().sendMessage("Sorry, can't use the TextToEmoji command since there was an error tyring to get the Char -> Emoji table");
+            return;
         }
 
+        String message = Strings.join(args, " ");
+        String result = toEmoji(message);
+
         MessageEditor.edit(((MessageCommandCaller) caller).getUser(), ((MessageCommandCaller) caller).getConversation(), ((MessageCommandCaller) caller).getMessage(), result, false);
+    }
+
+    public static String toEmoji(String text)
+    {
+        ArrayList<TextEmoji> emojis = new ArrayList<>();
+        String result = "";
+
+        for (char c : text.toCharArray())
+        {
+            if (!CHAR_TO_EMOJI.containsKey(c))
+            {
+                result += c;
+            }
+            else
+            {
+                result += CHAR_TO_EMOJI.get(c)[0].getUnicode();
+            }
+
+            result += " ";
+        }
+
+        return result;
+    }
+
+    private static HashMap<Character, TextEmoji[]> CHAR_TO_EMOJI;
+
+    static
+    {
+        try
+        {
+            CHAR_TO_EMOJI = (HashMap<Character, TextEmoji[]>) TextEmoji.Companion.getClass().getField("CHAR_TO_EMOJI").get(null);
+        }
+        catch (Exception ignored)
+        {
+        }
     }
 }
